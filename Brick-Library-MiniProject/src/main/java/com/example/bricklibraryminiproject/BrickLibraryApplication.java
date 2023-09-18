a65067a899b7eb7bceee79e858775045eb81d696
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-18 19:20:12 -0400

    Add Spring Boot and security dependencies

---
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/BrickLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.bricklibraryminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrickLibraryApplication.class, args);
+    }
+
+}
---
+++ Brick-Library-MiniProject/src/test/java/com/example/bricklibraryminiproject/BrickLibraryMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bricklibraryminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickLibraryMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
