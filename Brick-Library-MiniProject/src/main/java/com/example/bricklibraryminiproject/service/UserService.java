48b1d22e7324c5d2e042980321648a4102c3f14d
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 16:09:47 -0400

    Add comments to UserService for clarity

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
@@ -9,28 +9,53 @@ import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
+/**
+ * Service class responsible for user-related operations.
+ */
 @Service
 public class UserService {
 
+    // Logger instance to log events related to UserService operations
     private static final Logger logger = Logger.getLogger(UserService.class.getName());
+
+    // Repository to interact with user data in the database
     private final UserRepository userRepository;
+
+    // Encoder for password encryption
     private final PasswordEncoder passwordEncoder;
 
+
+    /**
+     * Constructor to initialize UserRepository and PasswordEncoder
+     *
+     * @param userRepository Repository for user-related database operations
+     * @param passwordEncoder Encoder for user password
+     */
     @Autowired
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+
+    /**
+     * Registers a new user
+     *
+     * @param userData User data to register
+     * @return The registered user
+     * @throws UserAlreadyExistsException if a user with the same email already exists
+     */
     public User registerNewUser(User userData) {
         if (userRepository.isEmailRegistered(userData.getEmailAddress())) {
 
             logger.warning("An attempt to register with existing email, " + userData.getEmailAddress() + ", occurred.");
             throw new UserAlreadyExistsException("Email, " + userData.getEmailAddress() + ", is already registered.");
         }
+        // Encrypt the user password before saving to the database
         userData.setPassword(passwordEncoder.encode(userData.getPassword()));
         logger.info("User with email, " + userData.getEmailAddress() + ", has successfully registered.");
 
+        // Save the user data in the database
         return userRepository.save(userData);
     }
 }
