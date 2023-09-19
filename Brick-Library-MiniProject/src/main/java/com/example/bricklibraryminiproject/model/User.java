bd74e80a0faa764618417c58876a4a1445b62dcc
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 12:53:25 -0400

    Add toString method to User model

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
@@ -61,4 +61,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
