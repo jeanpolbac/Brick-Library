6824eb61fe5175f987268163c1a4da25dc158258
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 18:04:34 -0400

    Add Logger to JwtUtils for improved logging

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/JwtUtils.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/JwtUtils.java
@@ -2,6 +2,13 @@ package com.example.bricklibraryminiproject.security;
 
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class JwtUtils {
+    private static final Logger logger = Logger.getLogger(JwtUtils.class.getName());
+
+    
+
+
 }
