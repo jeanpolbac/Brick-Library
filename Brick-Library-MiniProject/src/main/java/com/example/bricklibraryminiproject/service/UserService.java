0779f7393dfa9b65ae6dbb71e683b2282d12a048
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 15:56:22 -0400

    Add loggin to UserService and handle email registration checks

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class UserService {
+
+    private static final Logger logger = Logger.getLogger(UserService.class.getName());
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -20,9 +24,12 @@ public class UserService {
 
     public User registerNewUser(User userData) {
         if (userRepository.isEmailRegistered(userData.getEmailAddress())) {
+
+            logger.warning("An attempt to register with existing email, " + userData.getEmailAddress() + ", occurred.");
             throw new UserAlreadyExistsException("Email, " + userData.getEmailAddress() + ", is already registered.");
         }
         userData.setPassword(passwordEncoder.encode(userData.getPassword()));
+        logger.info("User with email, " + userData.getEmailAddress() + ", has successfully registered.");
 
         return userRepository.save(userData);
     }
