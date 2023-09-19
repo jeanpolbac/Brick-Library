7646ed593ecd371eb2a97173208cbe46d6c90255
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 15:38:19 -0400

    Implement registerNewUser method in UserService with custom exception handling

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/exception/UserAlreadyExistsException.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/exception/UserAlreadyExistsException.java
@@ -5,4 +5,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
+
+    // Constructor that accepts the error message
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
 }
--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.bricklibraryminiproject.service;
 
+import com.example.bricklibraryminiproject.exception.UserAlreadyExistsException;
+import com.example.bricklibraryminiproject.model.User;
 import com.example.bricklibraryminiproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,5 +16,14 @@ public class UserService {
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerNewUser(User userData) {
+        if (userRepository.isEmailRegistered(userData.getEmailAddress())) {
+            throw new UserAlreadyExistsException("Email, " + userData.getEmailAddress() + ", is already registered.");
+        }
+        userData.setPassword(passwordEncoder.encode(userData.getPassword()));
+
+        return userRepository.save(userData);
     }
 }
