87fd711be77249de45e48c5c9b26e50d9c847b13
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 12:27:05 -0400

    Add basic properties to User model

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
@@ -1,4 +1,15 @@
 package com.example.bricklibraryminiproject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    private Long id;
+    private String username;
+    private String emailAddress;
+    private String password;
+
 }
