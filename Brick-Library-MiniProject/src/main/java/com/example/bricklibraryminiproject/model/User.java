31d25ce521992cf1a3226aad12b011fbcab903c3
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 13:04:28 -0400

    Add comments to User model for clarity

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
@@ -6,23 +6,39 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
+    // Primary key of the User entity
     @Id
+    // Id property is mapped to database column
     @Column
+    // How the primary key will be generated
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Username field mapped to database column
     @Column
     private String username;
 
+    // Email set to be unique, cannot exist more than once. Email field mapped to database column
     @Column(unique = true)
     private String emailAddress;
 
+    // Password field mapped to database column
     @Column
     private String password;
 
+
+    // Default Constructor
     public User() {
     }
 
+    /**
+     * Constructs a User object with the provided parameters
+     *
+     * @param id            Unique identifier for the user
+     * @param username      Display name of the user
+     * @param emailAddress  Email address of the user
+     * @param password      Password associated with the user account
+     */
     public User(Long id, String username, String emailAddress, String password) {
         this.id = id;
         this.username = username;
@@ -30,6 +46,7 @@ public class User {
         this.password = password;
     }
 
+    // Getter and Setter methods for the User class attributes
     public Long getId() {
         return id;
     }
@@ -62,6 +79,7 @@ public class User {
         this.password = password;
     }
 
+    // Customized toString method for structured output.
     @Override
     public String toString() {
         return "User{" +
