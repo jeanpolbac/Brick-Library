5dfae84fd83ee39dcbeb56fecbf6fbefe332b77f
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 13:32:34 -0400

    Add findByEmailAddress method to UserRepository

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/UserRepository.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/UserRepository.java
@@ -4,7 +4,13 @@ import com.example.bricklibraryminiproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailAddress(String emailAddress);
+
+    
 
 }
