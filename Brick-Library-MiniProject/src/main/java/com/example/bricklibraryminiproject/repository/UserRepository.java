e57af2f4bc5782d74b9692a155374771aae18e79
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 13:38:55 -0400

    Add comments to UserRepository for clarity

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/UserRepository.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/UserRepository.java
@@ -6,10 +6,25 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+// Spring Data JPA repository interface
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Find a User entity by its email address
+     *
+     * @param emailAddress The email address associated with a user
+     * @return An Optional containing the User entity if found, or an empty Optional if no User is found with the given email address.
+     */
     Optional<User> findByEmailAddress(String emailAddress);
+
+
+    /**
+     * Checks if a User entity with the given email address exists
+     *
+     * @param emailAddress The email address to check
+     * @return true if a User entity with the given email address exists, false otherwise
+     */
 
     boolean existsByEmailAddress(String emailAddress);
 
