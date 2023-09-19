468e5b89d924bce407432e14d05de60c61b3cd87
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 18:12:33 -0400

    Add JWT configuration values via @Value annotations in JwtUtils

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/JwtUtils.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.example.bricklibraryminiproject.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -8,7 +9,12 @@ import java.util.logging.Logger;
 public class JwtUtils {
     private static final Logger logger = Logger.getLogger(JwtUtils.class.getName());
 
-    
+
+    @Value("${jwt-secret}")
+    private String jwtSecretKey;
+
+    @Value("${jwt-expiration-ms}")
+    private String jwtTokenExpiration;
 
 
 }
