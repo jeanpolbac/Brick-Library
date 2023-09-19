a989c9cae90a2a88d13e407387e127e65e6a42a5
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 15:03:43 -0400

    Add custom constructor to UserService for dependency injection

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/UserService.java
@@ -1,14 +1,18 @@
 package com.example.bricklibraryminiproject.service;
 
 import com.example.bricklibraryminiproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-   private final PasswordEncoder passwordEncoder;
-
-
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
