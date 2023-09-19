7238be8645899b1faefa89fdefdea844ad3c6277
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 12:52:23 -0400

    Add getters and setters to User model

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
@@ -22,11 +22,43 @@ public class User {
 
     public User() {
     }
-    
+
     public User(Long id, String username, String emailAddress, String password) {
         this.id = id;
         this.username = username;
         this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
