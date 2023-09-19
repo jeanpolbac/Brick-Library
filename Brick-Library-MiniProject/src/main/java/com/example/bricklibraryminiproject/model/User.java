9dc380beafc0dbdee4e1ac68d53a47c7ec72e31e
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 12:46:40 -0400

    Add JPA annotations to User model properties

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/User.java
@@ -1,15 +1,23 @@
 package com.example.bricklibraryminiproject.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String username;
+
+    @Column(unique = true)
     private String emailAddress;
+
+    @Column
     private String password;
 
 }
