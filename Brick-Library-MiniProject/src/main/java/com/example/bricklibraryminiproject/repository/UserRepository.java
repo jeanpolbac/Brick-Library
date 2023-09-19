ca3f0f52e3b1e35fad4cad6a16aa8ea39f519e7b
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 16:04:08 -0400

    Rename existsByEmailAddress to isEmailRegistered in UserRepository

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/UserRepository.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/UserRepository.java
@@ -26,6 +26,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * @return true if a User entity with the given email address exists, false otherwise
      */
 
-    boolean existsByEmailAddress(String emailAddress);
+    boolean isEmailRegistered(String emailAddress);
 
 }
