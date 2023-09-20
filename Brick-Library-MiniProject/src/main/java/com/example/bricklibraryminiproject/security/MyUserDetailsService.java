ee8f60214950e425415d91ce3c804fe24f0b4e67
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 21:51:24 -0400

    Implement loadUserByUsername method in MyUserDetailsService

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/MyUserDetailsService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/MyUserDetailsService.java
@@ -1,13 +1,20 @@
 package com.example.bricklibraryminiproject.security;
 
+import com.example.bricklibraryminiproject.model.User;
 import com.example.bricklibraryminiproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    private static final Logger logger = Logger.getLogger(MyUserDetailsService.class.getName());
     private final UserService userService;
 
     @Autowired
@@ -15,4 +22,12 @@ public class MyUserDetailsService implements UserDetailsService {
         this.userService = userService;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        Optional<User> user = userService.findByEmailAddress(emailAddress);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with provided email address " + emailAddress);
+        }
+        return new MyUserDetails(user.get());
+    }
 }
