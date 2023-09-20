6f0398586aa39e242344264a8f77e63d8c65c9ea
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 21:33:44 -0400

    Implemented MyUserDetailsService with dependency on UserService

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/MyUserDetailsService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/MyUserDetailsService.java
@@ -1,4 +1,18 @@
 package com.example.bricklibraryminiproject.security;
 
-public class MyUserDetailsService {
+import com.example.bricklibraryminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public MyUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
 }
