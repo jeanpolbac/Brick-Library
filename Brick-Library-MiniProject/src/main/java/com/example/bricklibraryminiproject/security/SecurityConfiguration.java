65c9891a6e5fa56968823a88ccf658c8653ea818
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 00:27:14 -0400

    Add comments and configure AuthenticationManager in SecurityConfiguration

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/SecurityConfiguration.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/SecurityConfiguration.java
@@ -2,8 +2,11 @@ package com.example.bricklibraryminiproject.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,11 +30,25 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/h2-console/**", "/auth/users/**").permitAll()
                 .anyRequest().authenticated()
+                .and().sessionManagement() // manage user session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable()
-                .httpBasic();
+                .headers().frameOptions().disable(); // allow to display iframe.
         return http.build();
     }
 
+
+    /**
+     * Defines a Spring Bean for configuring and providing the AuthenticationManager
+     *
+     * @param authConfig The AuthenticationConfiguration to obtain the AuthenticationManager
+     * @return An instance of AuthenticationManager configured by AuthenticationConfiguration
+     * @throws Exception If there's an error obtaining the AuthenticationManager
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
 
     /**
      * Provides an instance of BCryptPasswordEncoder
