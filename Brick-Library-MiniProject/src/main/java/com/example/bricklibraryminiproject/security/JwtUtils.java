bd7bc844d4a0d837ec75697d7ad7182deba5a61d
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 23:13:04 -0400

    Implement JWT token creation in generateJwtToken method

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/JwtUtils.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/security/JwtUtils.java
@@ -1,8 +1,11 @@
 package com.example.bricklibraryminiproject.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
@@ -14,7 +17,30 @@ public class JwtUtils {
     private String jwtSecretKey;
 
     @Value("${jwt-expiration-ms}")
-    private String jwtTokenExpiration;
+    private long jwtTokenExpiration;
 
 
+    /**
+     * Generates a JWT token based on user details
+     *
+     * @param userDetails The user details used to create the token
+     * @return A JWT token as a String
+     */
+    public String generateJwtToken(MyUserDetails userDetails) {
+        return Jwts.builder()
+                // Set the subject (usually the username)
+                .setSubject(userDetails.getUsername())
+
+                // Set the token's issuance time
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+
+                // Set the token's expiration time
+                .setExpiration(new Date(System.currentTimeMillis() + jwtTokenExpiration))
+
+                // Sign the token with the secret key using SHA-256
+                .signWith(SignatureAlgorithm.HS256, jwtSecretKey)
+
+                // Compact and return the JWT token as a String
+                .compact();
+    }
 }
