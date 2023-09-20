9ec416103721723cd3dc92219ef9bfad1c1862a7
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 22:21:02 -0400

    Add LoginResponse model for user authentication

---
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/response/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.example.bricklibraryminiproject.model.response;
+
+public class LoginResponse {
+
+    private String jwtToken;
+
+    public LoginResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    
+    //Getter and Setter
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+}
