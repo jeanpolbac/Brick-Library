9d01bcaf71e4a8f7fcd1534b4d9fcc0c59f6cfb0
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-19 22:53:02 -0400

    Added loginUser endpoint to handle authentication and JWT token creatoin in UserController

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/controller/UserController.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.example.bricklibraryminiproject.controller;
 
 import com.example.bricklibraryminiproject.model.User;
+import com.example.bricklibraryminiproject.model.request.LoginRequest;
+import com.example.bricklibraryminiproject.model.response.LoginResponse;
 import com.example.bricklibraryminiproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.logging.Logger;
 
 /**
  * This controller handles user-related HTTP requests, such as user registration.
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth/users")
 public class UserController {
+
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
 
     // Service to manage user data and operations
     private final UserService userService;
@@ -38,4 +47,16 @@ public class UserController {
     public User registerUser(@RequestBody User userData) {
         return userService.registerNewUser(userData);
     }
+
+
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        Optional<String> jwtToken = userService.authenticateAndGenerateToken(loginRequest);
+
+        if (jwtToken.isPresent()) {
+            return  ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed!"));
+        }
+    }
+
 }
