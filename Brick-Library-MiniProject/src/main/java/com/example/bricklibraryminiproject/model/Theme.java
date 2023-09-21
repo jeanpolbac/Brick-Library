0fbaffcb28b54ed8b93547a6f3a1393627517711
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 21:11:19 -0400

    Add getters and setters to Theme model

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/Theme.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/Theme.java
@@ -22,5 +22,19 @@ public class Theme {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
