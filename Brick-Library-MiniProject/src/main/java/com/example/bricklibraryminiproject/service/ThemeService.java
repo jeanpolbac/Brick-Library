14f7fbd8c1c2349535b46a8be4b816ab427fe9f6
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 23:25:27 -0400

    Add 'createTheme' method to ThemeService

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/ThemeService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/ThemeService.java
@@ -1,12 +1,17 @@
 package com.example.bricklibraryminiproject.service;
 
+import com.example.bricklibraryminiproject.exception.AlreadyExistsException;
+import com.example.bricklibraryminiproject.model.Theme;
 import com.example.bricklibraryminiproject.repository.LegoSetRepository;
 import com.example.bricklibraryminiproject.repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class ThemeService {
+    private static final Logger logger = Logger.getLogger(ThemeService.class.getName());
     private ThemeRepository themeRepository;
     private LegoSetRepository legoSetRepository;
 
@@ -18,5 +23,20 @@ public class ThemeService {
     @Autowired
     public void setLegoSetRepository(LegoSetRepository legoSetRepository) {
         this.legoSetRepository = legoSetRepository;
+    }
+
+    public Theme createTheme(Theme theme) {
+        try {
+            Theme existingTheme = themeRepository.findByName(theme.getName());
+            if (existingTheme != null) {
+                 logger.severe("Failed to create theme " + theme.getName() + ", because it already exists.");
+                throw new AlreadyExistsException("Theme with name '" + theme.getName() + "' already exists.");
+            }
+
+            return themeRepository.save(theme);
+        } catch (Exception e) {
+            logger.severe("Failed to create theme " + e.getMessage());
+            throw new RuntimeException("Failed to create theme " + e.getMessage(), e);
+        }
     }
 }
