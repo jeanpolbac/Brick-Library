222c9ecf7ce295fca884717f2014710450143843
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 21:31:29 -0400

    Add @Id, @Column, @GeneratedValue annotations to LegoSet model

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/LegoSet.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/model/LegoSet.java
@@ -1,16 +1,26 @@
 package com.example.bricklibraryminiproject.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "legosets")
 public class LegoSet {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
+
+    @Column
     private String theme;
+
+    @Column
     private int pieceCount;
+
+    @Column
     private int releaseYear;
 
 }
