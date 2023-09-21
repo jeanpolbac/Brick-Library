64d78c6887fc16c6fc5257cb76d2fbb5f8a555a4
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 22:23:34 -0400

    Add LegoSetRepository interface to manage LegoSet entity data

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/LegoSetRepository.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/LegoSetRepository.java
@@ -1,4 +1,9 @@
 package com.example.bricklibraryminiproject.repository;
 
-public interface LegoSetRepository {
+import com.example.bricklibraryminiproject.model.LegoSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LegoSetRepository extends JpaRepository<LegoSet, Long> {
 }
