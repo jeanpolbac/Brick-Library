32b89088f8b57a783d4a77960b8bf0e07d66501a
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 22:30:16 -0400

    Rename LegoSetRepository to ThemeRepository

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/ThemeRepository.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/ThemeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LegoSetRepository extends JpaRepository<LegoSet, Long> {
+public interface ThemeRepository extends JpaRepository<LegoSet, Long> {
 }
