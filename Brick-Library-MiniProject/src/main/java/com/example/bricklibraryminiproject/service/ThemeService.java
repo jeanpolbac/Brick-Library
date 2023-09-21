18f32af0afc79230fea18e9e97c3634de2684300
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 22:45:58 -0400

    Add autowired ThemeRepository and LegoSetRepository to ThemeService

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/ThemeService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/ThemeService.java
@@ -1,4 +1,22 @@
 package com.example.bricklibraryminiproject.service;
 
+import com.example.bricklibraryminiproject.repository.LegoSetRepository;
+import com.example.bricklibraryminiproject.repository.ThemeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ThemeService {
+    private ThemeRepository themeRepository;
+    private LegoSetRepository legoSetRepository;
+
+    @Autowired
+    public void setThemeRepository(ThemeRepository themeRepository) {
+        this.themeRepository = themeRepository;
+    }
+
+    @Autowired
+    public void setLegoSetRepository(LegoSetRepository legoSetRepository) {
+        this.legoSetRepository = legoSetRepository;
+    }
 }
