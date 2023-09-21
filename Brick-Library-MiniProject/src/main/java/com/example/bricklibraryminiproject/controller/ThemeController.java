e626a0c4dd9f58419cab4bfb5365fb022b19c28e
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-21 00:03:46 -0400

    Add 'createTheme' endpoint to ThemeController

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/controller/ThemeController.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/controller/ThemeController.java
@@ -1,4 +1,12 @@
 package com.example.bricklibraryminiproject.controller;
 
+
+import com.example.bricklibraryminiproject.service.ThemeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // http://localhost:9022/api
 public class ThemeController {
+    private ThemeService themeService;
 }
