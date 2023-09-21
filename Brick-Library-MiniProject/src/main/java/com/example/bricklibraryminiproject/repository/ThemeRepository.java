9bfc2927cc76560332c3d5b6b01969e5df782bef
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-20 22:50:18 -0400

    Add 'findByName' method to ThemeRepository

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/ThemeRepository.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/repository/ThemeRepository.java
@@ -1,9 +1,10 @@
 package com.example.bricklibraryminiproject.repository;
 
-import com.example.bricklibraryminiproject.model.LegoSet;
+import com.example.bricklibraryminiproject.model.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThemeRepository extends JpaRepository<LegoSet, Long> {
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+    Theme findByName(String themeName);
 }
