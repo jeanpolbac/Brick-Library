71b92ff389f096e1606ad32152972df77068d09c
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-21 23:17:22 -0400

    Add updateTheme method. Add @Preauthorize to createTheme and updateTheme

--- Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/ThemeService.java
+++ Brick-Library-MiniProject/src/main/java/com/example/bricklibraryminiproject/service/ThemeService.java
@@ -1,13 +1,16 @@
 package com.example.bricklibraryminiproject.service;
 
 import com.example.bricklibraryminiproject.exception.AlreadyExistsException;
+import com.example.bricklibraryminiproject.exception.NotFoundException;
 import com.example.bricklibraryminiproject.model.Theme;
 import com.example.bricklibraryminiproject.repository.LegoSetRepository;
 import com.example.bricklibraryminiproject.repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 /**
@@ -46,19 +49,39 @@ public class ThemeService {
      * @param theme The theme to create
      * @return The created theme
      */
+
+    @PreAuthorize("hasRole('ROLE_USER')")
     public Theme createTheme(Theme theme) {
         try {
             Theme existingTheme = themeRepository.findByName(theme.getName());
             if (existingTheme != null) {
-                 logger.severe("Failed to create theme " + theme.getName() + ", because it already exists.");
+                logger.severe("Failed to create theme " + theme.getName() + ", because it already exists.");
                 throw new AlreadyExistsException("Theme with name '" + theme.getName() + "' already exists.");
             }
-
             return themeRepository.save(theme);
         } catch (Exception e) {
             logger.severe("Failed to create theme " + e.getMessage());
             throw new RuntimeException("Failed to create theme " + e.getMessage(), e);
         }
+    }
+
+    /**
+     * Updates an existing theme based on its ID, but only if the authenticated user is the owner of the theme
+     *
+     * @param id The ID of the theme to update
+     * @param updatedTheme The updated theme details
+     * @return The updated theme
+     * @throws NotFoundException If the theme with the given ID is not found
+     */
+    @PreAuthorize("hasRole('ROLE_USER') and #theme.owner.username == authentication.principal.username")
+    public Theme updateTheme(Long id, Theme updatedTheme) {
+        Optional<Theme> existingThemeOptional = themeRepository.findById(id);
+        if (existingThemeOptional.isEmpty()) {
+            throw new NotFoundException("Theme with ID " + id + " not found!");
+        }
+        Theme existingTheme = existingThemeOptional.get();
+        existingTheme.setName(updatedTheme.getName());
+        return themeRepository.save(existingTheme);
     }
 
     /**
